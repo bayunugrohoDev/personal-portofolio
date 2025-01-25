e9834146bb8029ad33270ca8b038672de9c45217
M Bayu Dwi Nugroho <bayunugroho963@gmail.com>
2025-01-25 11:34:11 +0700

    ok

---
+++ src/components/Buy.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Buy = () => {
+  return (
+    <div className="fixed bottom-6 left-6">
+      <a
+        href="https://lynk.id/dunia.developer/w9R31rW/checkout"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button
+          type="button"
+          className="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-6 py-2 text-center text-white glass-effect animate-bounce"
+        >
+          I want it
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default Buy;
